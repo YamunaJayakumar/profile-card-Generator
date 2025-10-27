9d289d94bd5be209966c851e6c80e1800eab4880
YamunaJayakumar <yamunajayakumar789@gmail.com>
2025-10-27 22:17:58 +0530

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage'
+import History from './pages/History'
+import Pnf from './pages/Pnf'
+import Forms from './pages/Forms'
+import ProfileCardGenerator from './pages/ProfileCardGenerator'
+
 
 function App() {
   return (
     
-      <div style={{height:'100vh'}}>
-         <img src="\pngtree-id-card-icon-vector-png-image_20945759.png" alt="" />
-      </div>
+      <>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+        <Route path='/profilecard' element={<ProfileCardGenerator/>}/>
+        <Route path='/forms' element={<Forms/>}/>
+        <Route path='/history' element={<History/>}/>
+        <Route path='/*' element={<Pnf/>}/>
+      </Routes>
+      <Footer/>
+      </>
   
   );
 }
 
 export default App
+
+
---
+++ src/pages/ProfileCardGenerator.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProfileCardGenerator() {
+  return (
+    <div>ProfileCardGenerator</div>
+  )
+}
+
+export default ProfileCardGenerator
